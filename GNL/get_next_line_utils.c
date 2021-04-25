162eecfac320efd9ae6180dd730dbcfdf0d182e1
dchonvil <dchonvil@student.42.fr>
2021-04-25 04:31:07 +0200

    GNL OK

---
+++ GNL/get_next_line_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchonvil <dchonvil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 09:36:02 by dchonvil          #+#    #+#             */
+/*   Updated: 2021/04/25 02:50:30 by dchonvil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	saver(char *save, char *box, int c)
+{
+	int	i;
+
+	i = 0;
+	while (save[c] != '\0')
+	{
+		box[i] = save[c];
+		c++;
+		i++;
+	}
+	box[i] = '\0';
+}
+
+char	*save_treat(char *save)
+{
+	int		c;
+	int		i;
+	char	*box;
+
+	if (!save)
+		return (NULL);
+	c = 0;
+	i = 0;
+	while (save[c] != '\0' && save[c] != '\n')
+		c++;
+	if (!save[c])
+	{
+		free(save);
+		return (NULL);
+	}
+	while (save[i] != '\0')
+		i++;
+	if (!(box = malloc(sizeof(char) * ((i - c) + 1))))
+		return (NULL);
+	c++;
+	saver(save, box, c);
+	free(save);
+	return (box);
+}
+
+char	*render(char *save)
+{
+	int		c;
+	char	*box;
+
+	if (!save)
+		return (NULL);
+	c = 0;
+	while (save[c] && save[c] != '\n')
+		c++;
+	if (!(box = malloc(sizeof(char) * (c + 1))))
+		return (NULL);
+	c = 0;
+	while (save[c] && save[c] != '\n')
+	{
+		box[c] = save[c];
+		c++;
+	}
+	box[c] = '\0';
+	return (box);
+}
---
+++ GNL_bonus/get_next_line_bonus.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchonvil <dchonvil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 02:45:35 by dchonvil          #+#    #+#             */
+/*   Updated: 2021/04/25 04:12:23 by dchonvil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int		check_next_line(char *save)
+{
+	int	c;
+
+	if (!save)
+		return (0);
+	c = 0;
+	while (save[c])
+	{
+		if (save[c] == '\n')
+			return (1);
+		c++;
+	}
+	return (0);
+}
+
+int		count_char(char *str)
+{
+	int	c;
+
+	if (!str)
+		return (0);
+	c = 0;
+	while (str[c] != '\0')
+		c++;
+	return (c);
+}
+
+void	*copy_mem(void *box, const void *src, int n)
+{
+	int	c;
+
+	if (src == box)
+		return (box);
+	c = 0;
+	if (src < box)
+		while (n--)
+			((char*)box)[n] = ((char*)src)[n];
+	else
+		while (c < n)
+		{
+			((char*)box)[c] = ((char*)src)[c];
+			c++;
+		}
+	return (box);
+}
+
+char	*treatment(char *save, char *l)
+{
+	int		c;
+	int		i;
+	int		size;
+	char	*box;
+
+	if (!save && !l)
+		return (NULL);
+	c = count_char(save);
+	i = count_char(l);
+	size = c + i + 1;
+	box = malloc(sizeof(char) * size);
+	if (!box)
+		return (NULL);
+	copy_mem(box, save, c);
+	copy_mem((box + c), l, i);
+	box[size - 1] = '\0';
+	free(save);
+	return (box);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	int			r;
+	char		*l;
+	static char	*save[5000];
+
+	if (!line || BUFFER_SIZE <= 0 || fd < 0)
+		return (-1);
+	if (!(l = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	r = 1;
+	while (r != 0 && check_next_line(save[fd]) == 0)
+	{
+		if ((r = read(fd, l, BUFFER_SIZE)) == -1)
+		{
+			free(l);
+			return (-1);
+		}
+		l[r] = '\0';
+		save[fd] = treatment(save[fd], l);
+	}
+	free(l);
+	*line = render(save[fd]);
+	save[fd] = save_treat(save[fd]);
+	if (r == 0)
+		return (0);
+	return (1);
+}
